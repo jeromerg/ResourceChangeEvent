83f9d3d35bd12f34a54617acbcad7b7502036da6
jerome rg <jeromerg@gmx.net>
2014-04-04 14:28:24 +0200

    initial code commit

---
+++ Example/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using Itschwabing.Libraries.ResourceChangeEvent;
+
+namespace Example
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LargeFont_Click(object sender, RoutedEventArgs e)
+        {
+            Resources["VariableFontSize"] = 20d;
+        }
+
+        private void SmallFont_Click(object sender, RoutedEventArgs e)
+        {
+            Resources["VariableFontSize"] = 8d;
+        }
+
+        private void OnResourceChanged_VariableFontSize(object sender, ResourceChangedEventArgs e)
+        {
+            MessageBox.Show("This is a popup triggered by an event handler that subscribed to ResourceChangeEventBehavior ResourceChanged event!!");
+        }
+    }
+}
---
+++ ResourceChangeEvent/ResourceChangedEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Itschwabing.Libraries.ResourceChangeEvent
+{
+    public class ResourceChangedEventArgs : EventArgs
+    {
+        public object OldValue { get; private set; }
+        public object NewValue { get; private set; }
+
+        public ResourceChangedEventArgs(object oldValue, object newValue)
+        {
+            OldValue = oldValue;
+            NewValue = newValue;
+        }
+    }
+}
