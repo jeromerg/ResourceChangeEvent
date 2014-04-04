e8139ae2f2a5c2f8ad33688e044d71c1f3aa247a
jerome rg <jeromerg@gmx.net>
2014-04-04 14:45:16 +0200

    formatting

--- ResourceChangeEvent/ResourceChangeEventBehavior.cs
+++ ResourceChangeEvent/ResourceChangeEventBehavior.cs
@@ -4,35 +4,32 @@ using System.Windows.Interactivity;
 
 namespace Itschwabing.Libraries.ResourceChangeEvent
 {
-    namespace Itschwabing.Libraries.ResourceChangeEvent
+    public class ResourceChangeEventBehavior : Behavior<FrameworkElement>
     {
-        public class ResourceChangeEventBehavior : Behavior<FrameworkElement>
+        public static readonly DependencyProperty ResourceProperty = DependencyProperty.Register(
+            "Resource", typeof(object), typeof(ResourceChangeEventBehavior), new PropertyMetadata(default(object), ResourceChangedCallback));
+
+        public event EventHandler<ResourceChangedEventArgs> ResourceChanged;
+
+        public object Resource
         {
-            public static readonly DependencyProperty ResourceProperty = DependencyProperty.Register(
-                "Resource", typeof(object), typeof(ResourceChangeEventBehavior), new PropertyMetadata(default(object), ResourceChangedCallback));
+            get { return GetValue(ResourceProperty); }
+            set { SetValue(ResourceProperty, value); }
+        }
 
-            public event EventHandler<ResourceChangedEventArgs> ResourceChanged;
+        private static void ResourceChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args)
+        {
+            var resourceChangeNotifier = dependencyObject as ResourceChangeEventBehavior;
+            if (resourceChangeNotifier == null)
+                return;
 
-            public object Resource
-            {
-                get { return GetValue(ResourceProperty); }
-                set { SetValue(ResourceProperty, value); }
-            }
+            resourceChangeNotifier.OnResourceChanged(new ResourceChangedEventArgs(args.OldValue, args.NewValue));
+        }
 
-            private static void ResourceChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args)
-            {
-                var resourceChangeNotifier = dependencyObject as ResourceChangeEventBehavior;
-                if (resourceChangeNotifier == null)
-                    return;
-
-                resourceChangeNotifier.OnResourceChanged(new ResourceChangedEventArgs(args.OldValue, args.NewValue));
-            }
-
-            private void OnResourceChanged(ResourceChangedEventArgs args)
-            {
-                EventHandler<ResourceChangedEventArgs> handler = ResourceChanged;
-                if (handler != null) handler(this, args);
-            }
+        private void OnResourceChanged(ResourceChangedEventArgs args)
+        {
+            EventHandler<ResourceChangedEventArgs> handler = ResourceChanged;
+            if (handler != null) handler(this, args);
         }
     }
 }
